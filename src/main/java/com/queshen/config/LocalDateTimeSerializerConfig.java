9c7d1893ad625c4fa2baa180715c39deb0997ef2
WinstonYv <1973325293@qq.com>
2024-05-09 14:49:17 +0800

    GAI

--- src/main/java/com/queshen/config/LocalDateTimeSerializerConfig.java
+++ src/main/java/com/queshen/config/LocalDateTimeSerializerConfig.java
@@ -1,75 +1,32 @@
 package com.queshen.config;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
  * @author WinstonYv
- * @create 2022/12/11 15:14
+ * @create 2024/2/24 15:13
  * @Description: Man can conquer nature
  **/
 @Configuration
 public class LocalDateTimeSerializerConfig {
 
-    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
-
-    private static final String DATE_PATTERN = "yyyy-MM-dd";
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
-        JavaTimeModule module = new JavaTimeModule();
-        LocalDateTimeDeserializer localDateTimeDeserializer = new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        module.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
-        return builder -> {
-            builder.simpleDateFormat(DATE_TIME_PATTERN);
-            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
-            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN)));
-            builder.modules(module);
-        };
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
     }
 
-    /**
-     * string转localDatetime
-     */
     @Bean
-    public Converter<String, LocalDateTime> localDateTimeConverter() {
-        return source -> {
-            if (source.trim().length() == 0) {
-                return null;
-            }
-            // 先尝试ISO格式: 2019-07-15T16:00:00
-            try {
-                return LocalDateTime.parse(source);
-            } catch (Exception e) {
-                return LocalDateTime.parse(source, DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
-            }
-        };
-    }
-
-    /**
-     * string转localDate
-     */
-    @Bean
-    public Converter<String, LocalDate> localDateConverter() {
-        return source -> {
-            if (source.trim().length() == 0) {
-                return null;
-            }
-            try {
-                return LocalDate.parse(source);
-            } catch (Exception e) {
-                return LocalDate.parse(source, DateTimeFormatter.ofPattern(DATE_PATTERN));
-            }
-        };
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
     }
 }
--- src/main/java/com/queshen/pojo/dto/PreparedRecordResultDTO.java
+++ src/main/java/com/queshen/pojo/dto/PreparedRecordResultDTO.java
@@ -16,5 +16,4 @@ public class PreparedRecordResultDTO {
     public String msg;
 
     public PreparedRecordDTO Data;
-
 }
